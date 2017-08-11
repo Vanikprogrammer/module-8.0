587b57c6711c76409b7cafaf6915f0a71ebe838c
Ivan <ngk-trade@mail.ru>
2017-08-11 23:00:25 +0300

    доделал

--- src/saleFlowers/FlowersSaver.java
+++ src/saleFlowers/FlowersSaver.java
@@ -16,6 +16,7 @@ public final class FlowersSaver {
                 writer.flush();
             }
             writer.close();
+            
 
         }catch (Exception e){
             e.printStackTrace();
